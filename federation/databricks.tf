8616778040e3c913f8e67bf5c871be7c3aaa7066
Liem Truong <liem.truong@petalcard.com>
2024-07-21 14:12:21 -0700

    Add catalog to terraform

--- federation/databricks.tf
+++ federation/databricks.tf
@@ -1,3 +1,7 @@
+########################
+# Create SQL warehouse
+########################
+
 resource "databricks_sql_endpoint" "this" {
   name             = "dw-2x-small"
   cluster_size     = "2X-Small"
@@ -8,19 +12,42 @@ resource "databricks_sql_endpoint" "this" {
   depends_on = [azurerm_databricks_workspace.this]
 }
 
+#############################
+# Creat external connection
+#############################
+
 resource "databricks_connection" "redshift" {
   name            = "redshift_sandbox"
   connection_type = "REDSHIFT"
-  comment         = "this is a connection to a Redshift sandbox"
+  comment         = "This is a connection to a Redshift sandbox"
   options = {
     host     = var.redshift_host
     port     = "5439"
     user     = "testuser"
     password = var.redshift_password
   }
+
   properties = {
     purpose = "testing"
   }
 
   depends_on = [azurerm_databricks_workspace.this]
+}
+
+#########################################
+# Creat catalog for external connection
+#########################################
+
+resource "databricks_catalog" "redshift_sandbox" {
+  name            = "redshift_sandbox"
+  connection_name = databricks_connection.redshift.name
+  comment         = "This catalog contains external data from Redshift"
+
+  options = {
+    "database" = "sandbox"
+  }
+
+  properties = {
+    purpose = "testing"
+  }
 }
